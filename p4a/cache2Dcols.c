199ddf6ea609a732229a339eed4ef5bc464dbc62
Ayandeep Hazra <ayandeephazra@hotmail.com>
2021-07-23 14:36:31 -0500

    p4a

---
+++ p4a/cache2Dcols.c
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Ayan Deep Hazra
+// Email:            ahazra2@wisc.edu
+// CS Login:         ayan
+//
+//////////////////////////// 80 columns wide ///////////////////////////////////
+
+#define ROWS 3000
+#define COLUMNS 500
+
+int arr2D[ROWS][COLUMNS];
+
+int main(){
+
+	int row;
+	int col;
+
+
+	for(col = 0; col < COLUMNS; col++){
+		for(row = 0; row < ROWS; row++){
+			arr2D[row][col] = row + col;
+		}
+
+	}
+
+	return 0;
+}
+

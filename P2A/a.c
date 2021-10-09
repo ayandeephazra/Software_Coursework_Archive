87ef1e663802e1e9ee72f4266a531e4cca4f431c
Ayandeep Hazra <ayandeephazra@hotmail.com>
2021-10-09 18:08:48 -0500

    Initial Commit p2a

---
+++ P2A/a.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[]){
+    return 0;
+}
